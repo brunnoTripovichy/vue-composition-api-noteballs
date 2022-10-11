64e592b2b10934c2da9dc643cffa251819104f61
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2022-10-11 15:23:31 -0400

    Some interactivity with notes

---
+++ src/components/notes/Note.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        {{ note?.content }}
+        <div class="has-text-right has-text-grey-light mt-2">
+          <small>{{ characterLenght }}</small>
+        </div>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item" @click.prevent="deleteNote(note.id)"
+        >Delete</a
+      >
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  note: Object,
+});
+
+const emit = defineEmits(["deleteNote"]);
+
+const characterLenght = computed(() => {
+  const length = props?.note?.content?.length;
+  const description = length > 1 ? "characters" : "character";
+  return `${length} ${description}`;
+});
+
+const deleteNote = (id) => {
+  emit("deleteNote", id);
+};
+</script>
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -3,32 +3,71 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="textareaNote"
+          />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            class="button is-link has-background-success"
+            @click="addNewNote()"
+            :disabled="!newNote"
+          >
             Add new note
           </button>
         </div>
       </div>
     </div>
 
-    <div class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-          reiciendis, nostrum quasi odit blanditiis ad consequuntur porro quos
-          commodi in hic at atque culpa doloribus eum dolores ducimus delectus
-          laudantium?
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <Note
+      v-for="note in notes"
+      :key="note?.id"
+      :note="note"
+      @deleteNote="deleteNote($event)"
+    />
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import Note from "@/components/notes/Note.vue";
+
+const textareaNote = ref(null);
+
+const notes = ref([
+  {
+    id: 1,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci reiciendis, nostrum quasi odit blanditiis ad consequuntur porro quos commodi in hic at atque culpa doloribus eum dolores ducimus delectus laudantium?",
+  },
+  {
+    id: 2,
+    content: "Smaller note",
+  },
+]);
+
+const newNote = ref("");
+
+const addNewNote = () => {
+  const note = {
+    id: new Date().getTime(),
+    content: newNote.value,
+  };
+
+  notes.value.push(note);
+  newNote.value = "";
+
+  textareaNote.value.focus();
+};
+
+const deleteNote = (id) => {
+  const objWithIdIndex = notes.value.findIndex((obj) => obj.id === id);
+  notes.value.splice(objWithIdIndex, 1);
+};
+</script>
